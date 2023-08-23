b189bcfc88f753bbfd96d4c2dd86d8f14922ceba
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-08-23 22:50:34 +0700

    GEDE

--- projects/server/src/index.js
+++ projects/server/src/index.js
@@ -1,14 +1,15 @@
 require("dotenv/config");
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const { join } = require("path");
 const path = require("path");
 
 const db = require("../models");
 // db.sequelize.sync({ alter: true });
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 const app = express();
+app.use(cors)
 // app.use(
 //   cors({
 //     origin: [
@@ -37,58 +38,62 @@ app.use(
   attendRouter,
   salaryRouter,
 );
-app.use("/public", express.static(path.resolve(__dirname, "../public")));
+// app.use("/public", express.static(path.resolve(__dirname, "../public")));
 
-app.get("/api", (req, res) => {
-  res.send(`Hello, this is my API`);
-});
+// app.get("/api", (req, res) => {
+//   res.send(`Hello, this is my API`);
+// });
 
-app.get("/api/greetings", (req, res, next) => {
-  res.status(200).json({
-    message: "Hello, Student !",
-  });
-});
+// app.get("/api/greetings", (req, res, next) => {
+//   res.status(200).json({
+//     message: "Hello, Student !",
+//   });
+// });
 
-app.get;
+// app.get;
 
 // ===========================
 
 // not found
-app.use((req, res, next) => {
-  if (req.path.includes("/api/")) {
-    res.status(404).send("Not found !");
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     res.status(404).send("Not found !");
+//   } else {
+//     next();
+//   }
+// });
 
 // error
-app.use((err, req, res, next) => {
-  if (req.path.includes("/api/")) {
-    console.error("Error : ", err.stack);
-    res.status(500).send("Error !");
-  } else {
-    next();
-  }
-});
+// app.use((err, req, res, next) => {
+//   if (req.path.includes("/api/")) {
+//     console.error("Error : ", err.stack);
+//     res.status(500).send("Error !");
+//   } else {
+//     next();
+//   }
+// });
 
 //#endregion
 
 //#region CLIENT
-const clientPath = "../../client/build";
-app.use(express.static(join(__dirname, clientPath)));
+// const clientPath = "../../client/build";
+// app.use(express.static(join(__dirname, clientPath)));
 
 // Serve the HTML page
-app.get("*", (req, res) => {
-  res.sendFile(join(__dirname, clientPath, "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(join(__dirname, clientPath, "index.html"));
+// });
 
 //#endregion
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.log(`ERROR: ${err}`);
-  } else {
-    console.log(`APP RUNNING at ${PORT} ✅`);
-  }
-});
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(`ERROR: ${err}`);
+//   } else {
+//     console.log(`APP RUNNING at ${PORT} ✅`);
+//   }
+// });
+
+app.listen(PORT, () => {
+  console.log(`jalan ${PORT}`)
+})
