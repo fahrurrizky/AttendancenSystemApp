1e72ba55aff8c912f2bd82bf41365de48ccacb86
M Fahrur Rizky <70459040+fahrurrizky@users.noreply.github.com>
2023-08-23 22:15:53 +0700

    port

--- projects/server/config/config.js
+++ projects/server/config/config.js
@@ -8,6 +8,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
@@ -15,6 +16,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   production: {
@@ -22,6 +24,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
 };
